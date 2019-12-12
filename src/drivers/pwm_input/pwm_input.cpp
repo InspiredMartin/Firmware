6f14d8e3fcb0ad6bac82bcb30eeb3a49dcd25673
Jacob Dahl <dahl.jakejacob@gmail.com>
2019-12-11 22:32:08 -0700

    Added header file, cleaned up to match stylistically.

--- src/drivers/pwm_input/pwm_input.cpp
+++ src/drivers/pwm_input/pwm_input.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2015 Andrew Tridgell. All rights reserved.
+ *   Copyright (c) 2019 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,180 +31,7 @@
  *
  ****************************************************************************/
 
-#include <drivers/drv_hrt.h>
-
-#include <uORB/uORB.h>
-#include <uORB/Publication.hpp>
-#include <uORB/topics/pwm_input.h>
-
-#include <drivers/device/device.h>
-
-
-#if HRT_TIMER == PWMIN_TIMER
-#error cannot share timer between HRT and PWMIN
-#endif
-
-#if !defined(GPIO_PWM_IN) || !defined(PWMIN_TIMER) || !defined(PWMIN_TIMER_CHANNEL)
-#error PWMIN defines are needed in board_config.h for this board
-#endif
-
-/* PWMIN configuration */
-#if   PWMIN_TIMER == 1
-# define PWMIN_TIMER_BASE	STM32_TIM1_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB2ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB2ENR_TIM1EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM1CC
-# define PWMIN_TIMER_CLOCK	STM32_APB2_TIM1_CLKIN
-#elif PWMIN_TIMER == 2
-# define PWMIN_TIMER_BASE	STM32_TIM2_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB1ENR_TIM2EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM2
-# define PWMIN_TIMER_CLOCK	STM32_APB1_TIM2_CLKIN
-#elif PWMIN_TIMER == 3
-# define PWMIN_TIMER_BASE	STM32_TIM3_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB1ENR_TIM3EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM3
-# define PWMIN_TIMER_CLOCK	STM32_APB1_TIM3_CLKIN
-#elif PWMIN_TIMER == 4
-# define PWMIN_TIMER_BASE	STM32_TIM4_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB1ENR_TIM4EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM4
-# define PWMIN_TIMER_CLOCK	STM32_APB1_TIM4_CLKIN
-#elif PWMIN_TIMER == 5
-# define PWMIN_TIMER_BASE	STM32_TIM5_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB1ENR_TIM5EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM5
-# define PWMIN_TIMER_CLOCK	STM32_APB1_TIM5_CLKIN
-#elif PWMIN_TIMER == 8
-# define PWMIN_TIMER_BASE	STM32_TIM8_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB2ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB2ENR_TIM8EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM8CC
-# define PWMIN_TIMER_CLOCK	STM32_APB2_TIM8_CLKIN
-#elif PWMIN_TIMER == 9
-# define PWMIN_TIMER_BASE	STM32_TIM9_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB2ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB2ENR_TIM9EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM1BRK
-# define PWMIN_TIMER_CLOCK	STM32_APB2_TIM9_CLKIN
-#elif PWMIN_TIMER == 10
-# define PWMIN_TIMER_BASE	STM32_TIM10_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB2ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB2ENR_TIM10EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM1UP
-# define PWMIN_TIMER_CLOCK	STM32_APB2_TIM10_CLKIN
-#elif PWMIN_TIMER == 11
-# define PWMIN_TIMER_BASE	STM32_TIM11_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB2ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB2ENR_TIM11EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM1TRGCOM
-# define PWMIN_TIMER_CLOCK	STM32_APB2_TIM11_CLKIN
-#elif PWMIN_TIMER == 12
-# define PWMIN_TIMER_BASE	STM32_TIM12_BASE
-# define PWMIN_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define PWMIN_TIMER_POWER_BIT	RCC_APB1ENR_TIM12EN
-# define PWMIN_TIMER_VECTOR	STM32_IRQ_TIM8BRK
-# define PWMIN_TIMER_CLOCK	STM32_APB1_TIM12_CLKIN
-#else
-# error PWMIN_TIMER must be a value between 1 and 12
-#endif
-
-/*
- * HRT clock must be at least 1MHz
- */
-#if PWMIN_TIMER_CLOCK <= 1000000
-# error PWMIN_TIMER_CLOCK must be greater than 1MHz
-#endif
-
-/*
- * Timer register accessors
- */
-#define REG(_reg)	(*(volatile uint32_t *)(PWMIN_TIMER_BASE + _reg))
-
-#define rCR1		REG(STM32_GTIM_CR1_OFFSET)
-#define rCR2		REG(STM32_GTIM_CR2_OFFSET)
-#define rSMCR		REG(STM32_GTIM_SMCR_OFFSET)
-#define rDIER		REG(STM32_GTIM_DIER_OFFSET)
-#define rSR		REG(STM32_GTIM_SR_OFFSET)
-#define rEGR		REG(STM32_GTIM_EGR_OFFSET)
-#define rCCMR1		REG(STM32_GTIM_CCMR1_OFFSET)
-#define rCCMR2		REG(STM32_GTIM_CCMR2_OFFSET)
-#define rCCER		REG(STM32_GTIM_CCER_OFFSET)
-#define rCNT		REG(STM32_GTIM_CNT_OFFSET)
-#define rPSC		REG(STM32_GTIM_PSC_OFFSET)
-#define rARR		REG(STM32_GTIM_ARR_OFFSET)
-#define rCCR1		REG(STM32_GTIM_CCR1_OFFSET)
-#define rCCR2		REG(STM32_GTIM_CCR2_OFFSET)
-#define rCCR3		REG(STM32_GTIM_CCR3_OFFSET)
-#define rCCR4		REG(STM32_GTIM_CCR4_OFFSET)
-#define rDCR		REG(STM32_GTIM_DCR_OFFSET)
-#define rDMAR		REG(STM32_GTIM_DMAR_OFFSET)
-
-/*
- * Specific registers and bits used by HRT sub-functions
- */
-#if PWMIN_TIMER_CHANNEL == 1
-#define rCCR_PWMIN_A		rCCR1			/* compare register for PWMIN */
-#define DIER_PWMIN_A		(GTIM_DIER_CC1IE) 	/* interrupt enable for PWMIN */
-#define SR_INT_PWMIN_A		GTIM_SR_CC1IF		/* interrupt status for PWMIN */
-#define rCCR_PWMIN_B		rCCR2 			/* compare register for PWMIN */
-#define SR_INT_PWMIN_B		GTIM_SR_CC2IF		/* interrupt status for PWMIN */
-#define CCMR1_PWMIN		((0x02 << GTIM_CCMR1_CC2S_SHIFT) | (0x01 << GTIM_CCMR1_CC1S_SHIFT))
-#define CCMR2_PWMIN		0
-#define CCER_PWMIN		(GTIM_CCER_CC2P | GTIM_CCER_CC1E | GTIM_CCER_CC2E)
-#define SR_OVF_PWMIN		(GTIM_SR_CC1OF | GTIM_SR_CC2OF)
-#define SMCR_PWMIN_1		(0x05 << GTIM_SMCR_TS_SHIFT)
-#define SMCR_PWMIN_2		((0x04 << GTIM_SMCR_SMS_SHIFT) | SMCR_PWMIN_1)
-#elif PWMIN_TIMER_CHANNEL == 2
-#define rCCR_PWMIN_A		rCCR2			/* compare register for PWMIN */
-#define DIER_PWMIN_A		(GTIM_DIER_CC2IE)	/* interrupt enable for PWMIN */
-#define SR_INT_PWMIN_A		GTIM_SR_CC2IF		/* interrupt status for PWMIN */
-#define rCCR_PWMIN_B		rCCR1			/* compare register for PWMIN */
-#define DIER_PWMIN_B		GTIM_DIER_CC1IE		/* interrupt enable for PWMIN */
-#define SR_INT_PWMIN_B		GTIM_SR_CC1IF		/* interrupt status for PWMIN */
-#define CCMR1_PWMIN		((0x01 << GTIM_CCMR1_CC2S_SHIFT) | (0x02 << GTIM_CCMR1_CC1S_SHIFT))
-#define CCMR2_PWMIN		0
-#define CCER_PWMIN		(GTIM_CCER_CC1P | GTIM_CCER_CC1E | GTIM_CCER_CC2E)
-#define SR_OVF_PWMIN		(GTIM_SR_CC1OF | GTIM_SR_CC2OF)
-#define SMCR_PWMIN_1		(0x06 << GTIM_SMCR_TS_SHIFT)
-#define SMCR_PWMIN_2		((0x04 << GTIM_SMCR_SMS_SHIFT) | SMCR_PWMIN_1)
-#else
-#error PWMIN_TIMER_CHANNEL must be either 1 and 2.
-#endif
-
-class PWMIN
-{
-public:
-	int init();
-
-	void publish(uint16_t status, uint32_t period, uint32_t pulse_width);
-	void print_info(void);
-
-private:
-	void _timer_init(void);
-
-	uint32_t _error_count{};
-	uint32_t _pulses_captured{};
-	uint32_t _last_period{};
-	uint32_t _last_width{};
-
-	bool _timer_started{};
-
-	pwm_input_s _pwm{};
-
-	uORB::PublicationData<pwm_input_s>	_pwm_input_pub{ORB_ID(pwm_input)};
-
-};
-
-static int pwmin_tim_isr(int irq, void *context, void *arg);
-static void pwmin_start();
-static void pwmin_info(void);
-static void pwmin_test(void);
-static void pwmin_usage(void);
+#include "pwm_input.h"
 
 static PWMIN *g_dev;
 
@@ -276,6 +103,22 @@ PWMIN::_timer_init(void)
 	up_enable_irq(PWMIN_TIMER_VECTOR);
 }
 
+static int pwmin_tim_isr(int irq, void *context, void *arg)
+{
+	uint16_t status = rSR;
+	uint32_t period = rCCR_PWMIN_A;
+	uint32_t pulse_width = rCCR_PWMIN_B;
+
+	/* ack the interrupts we just read */
+	rSR = 0;
+
+	if (g_dev != nullptr) {
+		g_dev->publish(status, period, pulse_width);
+	}
+
+	return OK;
+}
+
 void
 PWMIN::publish(uint16_t status, uint32_t period, uint32_t pulse_width)
 {
@@ -307,42 +150,33 @@ PWMIN::print_info(void)
 		 (unsigned)_last_width);
 }
 
-static int pwmin_tim_isr(int irq, void *context, void *arg)
+namespace pwm_input
 {
-	uint16_t status = rSR;
-	uint32_t period = rCCR_PWMIN_A;
-	uint32_t pulse_width = rCCR_PWMIN_B;
 
-	/* ack the interrupts we just read */
-	rSR = 0;
-
-	if (g_dev != nullptr) {
-		g_dev->publish(status, period, pulse_width);
-	}
-
-	return OK;
-}
-
-static void pwmin_start()
+static int start()
 {
 	if (g_dev != nullptr) {
-		errx(1, "driver already started");
+		PX4_ERR("driver already started");
+		return 1;
 	}
 
 	g_dev = new PWMIN();
 
 	if (g_dev == nullptr) {
-		errx(1, "driver allocation failed");
+		PX4_ERR("driver allocation failed");
+		return 1;
+
 	}
 
 	if (g_dev->init() != OK) {
-		errx(1, "driver init failed");
+		PX4_ERR("driver init failed");
+		return 1;
 	}
 
-	exit(0);
+	return 0;
 }
 
-static void pwmin_test(void)
+static int test(void)
 {
 	uint64_t start_time = hrt_absolute_time();
 
@@ -356,46 +190,48 @@ static void pwmin_test(void)
 		px4_usleep(20);
 	}
 
-	exit(0);
+	return 0;
 }
 
-static void pwmin_info(void)
+static int info(void)
 {
 	if (g_dev == nullptr) {
 		PX4_INFO("driver not started\n");
-		exit(1);
+		return 1;
 	}
 
 	g_dev->print_info();
-	exit(0);
+	return 0;
 }
 
-static void pwmin_usage()
+static int usage()
 {
 	PX4_ERR("unrecognized command, try 'start', 'info', 'reset' or 'test'");
+
+	return -1;
 }
+
+} // end namespace pwm_input
 
 extern "C" __EXPORT int pwm_input_main(int argc, char *argv[])
 {
 	if (argc < 2) {
-		pwmin_usage();
-		return -1;
+		return pwm_input::usage();
 	}
 
 	const char *verb = argv[1];
 
 	if (!strcmp(verb, "start")) {
-		pwmin_start();
+		return pwm_input::start();
 	}
 
 	if (!strcmp(verb, "info")) {
-		pwmin_info();
+		return pwm_input::info();
 	}
 
 	if (!strcmp(verb, "test")) {
-		pwmin_test();
+		return pwm_input::test();
 	}
 
-	pwmin_usage();
-	return -1;
+	return pwm_input::usage();
 }
